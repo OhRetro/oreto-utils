e71ea4eb7df8f3bd2424f69c70274f3e67f40856
OhRetro <gabrielnacazone@hotmail.com>
2022-02-19 19:41:42 -0300

    one change

--- pack_n_upload.py
+++ pack_n_upload.py
@@ -3,8 +3,5 @@
 from os import system
 
 if __name__ == "__main__":
-    try:
-        system("python3 setup.py sdist bdist_wheel")
-        system("twine upload dist/*")
-    except Exception as _:
-        print(_)
+    system("python3 setup.py sdist bdist_wheel")
+    system("twine upload dist/*")
