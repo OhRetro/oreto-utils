b8f89f31e13916d28baae21355f60d4121fc4d40
OhRetro <gabrielnacazone@hotmail.com>
2022-05-01 11:53:28 -0300

    0.5

--- oreto_utils/__init__.py
+++ oreto_utils/__init__.py
@@ -1,8 +1,9 @@
 __title__ = "oreto-utils"
 __author__ = "OhRetro"
 __license__ = "MIT"
-__version__ = "0.4"
+__version__ = "0.5"
 
 from .pyqt import *
 from .terminal import *
 from .file import *
+from .logger import *
