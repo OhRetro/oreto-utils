f3571ea0b4c00cff6f3833d4827b8e5ed26ee538
OhRetro <gabrielnacazone@hotmail.com>
2022-05-01 11:53:04 -0300

    add: clear_lines function (windows)

--- oreto_utils/terminal.py
+++ oreto_utils/terminal.py
@@ -1,6 +1,7 @@
 #Terminal
 
 from os import name as os_name, system as os_system
+import sys
 if os_name == "nt": import ctypes
 
 class Terminal:
@@ -8,7 +9,14 @@ class Terminal:
     def clear():
         if os_name == "nt": os_system("cls")
         else: os_system("clear")
-        
+    
+    #Clear the previous lines or the last line
+    def clear_lines(lines:int=1):
+        for _ in range(lines):
+            if os_name == "nt":
+                sys.stdout.write('\x1b[1A')
+                sys.stdout.write('\x1b[2K')
+     
     #Set title of the terminal
     def title(title:str):
         if os_name == "nt": ctypes.windll.kernel32.SetConsoleTitleW(title)
