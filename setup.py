6bdc888c114d4d1f764512c96d79506edd6c05f6
OhRetro <gabrielnacazone@hotmail.com>
2022-05-10 19:42:51 -0300

    0.6.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except Exception:
 
 setup(
     name = "oreto-utils",
-    version = "0.6.1.1",
+    version = "0.6.2",
     author = "OhRetro",
     author_email = "notareal@email.com",
     description = "a bunch of utilities to use in my code without copying and pasting the function from code to code",
